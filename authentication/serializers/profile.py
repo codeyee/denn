23c9e96c2b1700210f36c12be40088e1400e02d4
emlopezr <lopezrodemmanuel@gmail.com>
2025-10-28 20:52:50 -0500

    Enhancement: ProfileSerializer

--- authentication/serializers/profile.py
+++ authentication/serializers/profile.py
@@ -4,7 +4,6 @@ from django.db.models import Q
 from content.models import UserList, Rating
 from content.serializers import UserListSerializer, RatingSerializer
 
-
 class ProfileSerializer(serializers.ModelSerializer):
     lists = serializers.SerializerMethodField()
     ratings = serializers.SerializerMethodField()
@@ -29,26 +28,22 @@ class ProfileSerializer(serializers.ModelSerializer):
         read_only_fields = ['id']
 
     def get_lists(self, obj):
-        user_lists = UserList.objects.filter(
-            Q(owner=obj) | Q(members=obj)
-        ).distinct().prefetch_related(
+        q_filter = Q(owner=obj) | Q(members=obj)
+        user_lists = UserList.objects.filter(q_filter).distinct().prefetch_related(
             'members',
             'items__content_item',
             'items__added_by'
-        ).order_by(
-            '-created_at'
-        )
+        ).order_by('-created_at')
 
         return UserListSerializer(user_lists, many=True, context=self.context).data
 
     def get_ratings(self, obj):
-        user_ratings = Rating.objects.filter(user=obj).select_related(
-            'content_item').order_by('-created_at')
-
+        user_ratings = Rating.objects.filter(user=obj).select_related('content_item').order_by('-created_at')
         return RatingSerializer(user_ratings, many=True, context=self.context).data
 
     def get_lists_count(self, obj):
-        return UserList.objects.filter(Q(owner=obj) | Q(members=obj)).distinct().count()
+        q_filter = Q(owner=obj) | Q(members=obj)
+        return UserList.objects.filter(q_filter).distinct().count()
 
     def get_ratings_count(self, obj):
         return Rating.objects.filter(user=obj).count()
