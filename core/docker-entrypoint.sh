40a0ec2d4b009703b77d187d497353697d5c0369
Emmanuel López <45053254+emlopezr@users.noreply.github.com>
2026-07-29 18:38:11 -0500

    Run duration backfill once during deploy

--- core/docker-entrypoint.sh
+++ core/docker-entrypoint.sh
@@ -3,4 +3,11 @@ set -eu
 
 python manage.py migrate --noinput
 
+echo "Starting one-off game duration backfill"
+python manage.py rehydrate_content_details \
+  --content-type GAME \
+  --include-no-data \
+  --limit 500 \
+  --workers 4 &
+
 exec gunicorn --bind 0.0.0.0:8000 core.wsgi:application
